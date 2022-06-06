43b5c86f23554aa7ff38a9642902a382bf0217ab
John OHara <johara@redhat.com>
2022-06-06 12:37:44 +0100

    Update script to output params as well

--- src/test/resources/example.sh
+++ src/test/resources/example.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 val=$(echo $1*$2 | bc)
-echo "{\"data\": { \"data\": 1, \"count\": 100, \"average\": $val}}" > $(pwd)/data.json
+cat > $(pwd)/data.json <<- EOM
+{
+  "params": {
+    "memoryRequest": $1,
+    "cpuRequest": $2,
+    "parallel": $3
+  }
+  "output": {
+    "data": 1,
+    "count": 100,
+    "average": $val
+    }
+}
+EOM
 cat $(pwd)/data.json
