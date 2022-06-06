c9a97b369bd6576fcd23d2f76e0012401e8232f0
John OHara <johara@redhat.com>
2022-06-06 12:42:54 +0100

    Update script

--- src/test/resources/example.sh
+++ src/test/resources/example.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 val=$(echo $1*$2 | bc)
 cat > $(pwd)/data.json <<- EOM
-{
-  "params": {
-    "memoryRequest": $1,
-    "cpuRequest": $2,
-    "parallel": $3
-  }
-  "output": {
-    "data": 1,
-    "count": 100,
-    "average": $val
+{ "data": {
+    "params": {
+      "memoryRequest": $1,
+      "cpuRequest": $2,
+      "parallel": $3
     }
+    "output": {
+      "data": 1,
+      "count": 100,
+      "average": $val
+    }
+  }
 }
 EOM
 cat $(pwd)/data.json
